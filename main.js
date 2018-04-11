0ee59a19b0bbce12c31f6656b8aa2ce33956a15a
Peter Chenery <peter.chenery@anthonynolan.org>
2018-04-11 11:39:30 +0100

    Javascript function call

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 var tryButton = document.getElementById('try');
-tryButton.addEventListener('click', AddButton);
+tryButton.addEventListener('click', AddPara);
 
-function AddButton(){
-	var btn = document.createElement("BUTTON");
-	btn.id = "new";
-	var t = document.createTextNode("Click Me");
-	btn.appendChild(t);
-	document.body.appendChild(btn);
+function AddPara(){
+	var newp = document.createElement("para");
+	newp.id = "new";
+	var now = new Date();
+	if (now.getMonth() == 12 && now.getDate() == 25){
+		var t = document.createTextNode("It is Xmas!");
+	}else{
+		var t = document.createTextNode("It is not Xmas");
+	}
+	newp.appendChild(t);
+	document.body.appendChild(newp);
+	//alert(newp.id);
 }
 
 var clickButton = document.getElementById('delete');
@@ -14,4 +20,9 @@ clickButton.addEventListener('click', RemoveImage);
 
 function RemoveImage(){
 	document.getElementById('homer').style.display = 'none';
+
+//function isItChristmas(){
+//		var now = new Date();
+//		return (now.getMonth() == 12 && now.getDate() == 25);
+//	}
 }
